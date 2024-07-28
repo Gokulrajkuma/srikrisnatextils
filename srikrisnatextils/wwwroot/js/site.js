05c8522583add79f8758a591387d972922697bc1
gggok <gggok@GOKUL>
2024-07-28 23:08:57 +0530

    Add project files.

---
+++ srikrisnatextils/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('.nav-link');
+
+    function removeActiveClass() {
+        navLinks.forEach(link => link.classList.remove('active'));
+    }
+
+    function setActiveClass() {
+        const currentUrl = window.location.href;
+        navLinks.forEach(link => {
+            if (link.href === currentUrl) {
+                link.classList.add('active');
+            }
+        });
+    }
+
+    setActiveClass();
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            removeActiveClass();
+            link.classList.add('active');
+        });
+    });
+});
